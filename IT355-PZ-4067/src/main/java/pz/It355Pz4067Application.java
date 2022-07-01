ec6f658441edb4c3138e6939486860a915dc197c
lente9 <96121915+lente9@users.noreply.github.com>
2022-07-01 23:16:31 +0200

    Add files via upload

---
+++ IT355-PZ-4067/src/main/java/pz/It355Pz4067Application.java
@@ -0,0 +1,13 @@
+package pz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It355Pz4067Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(It355Pz4067Application.class, args);
+    }
+
+}
---
+++ IT355-PZ-4067/src/test/java/com/jovanovic/it355pz/It355Pz4067ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovanovic.it355pz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It355Pz4067ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
